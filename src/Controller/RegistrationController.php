5084bced23b20f3673e28e78c5cc81a48d91e209
doilus <nikalug@onet.eu>
2021-04-12 16:54:48 +0200

    send email after register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -4,16 +4,19 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
+    public function register(MailerInterface $mailer, Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -32,6 +35,20 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
             // do anything else you need here, like send an email
+
+            //send email
+            //NamedAddress - *exists* symfony 4.4 or less
+            $email = (new TemplatedEmail())
+                ->from('alienmailer@example.com')
+                ->to($user->getEmail())
+                ->subject('Welcome to the task list!')
+                ->htmlTemplate('email/welcome.html.twig')
+                ->context([
+                    'user' => $user
+                ])
+                ;
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('app_homepage');
         }
